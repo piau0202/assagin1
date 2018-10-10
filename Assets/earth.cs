f4510f3c6c5b91a2be87ff323d19857f5c5d9e06
piau0202 <31920931+piau0202@users.noreply.github.com>
2018-10-11 01:25:01 +0800

    Initial commit

---
+++ Assets/earth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public float y;
+
+    void Update()
+    {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, y * Time.deltaTime, 0);
+        transform.Rotate(angle);
+    }
+}
---
+++ Assets/moon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+
+    public float y;
+
+    void Update()
+    {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, y * Time.deltaTime, 0);
+        transform.Rotate(angle);
+    }
+}
